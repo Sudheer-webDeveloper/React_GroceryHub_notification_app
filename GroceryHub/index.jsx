cbcdd41d163457e0172dcca7083474dc13d621b6
Sudheer-webDeveloper <sudheertalaudi@gmail.com>
2023-10-28 11:32:27 +0530

    first commit

---
+++ GroceryHub/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Root from './Root'
+import './Grocery.css'
+
+
+function Index() {
+  return (
+    <div className='main-container'>
+      <Root />
+    </div>
+  )
+}
+
+export default Index
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Index from '../GroceryHub'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Index />
+    </>
+  )
+}
+
+export default App
